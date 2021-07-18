548c5824b92494e66100d55dde0ef27eee38f52d
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-07-18 13:34:54 -0400

    kata done

---
+++ Count of positives  sum of negatives/index.js
@@ -0,0 +1,20 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+// If the input array is empty or null, return an empty array.
+// Example
+
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+
+function countPositivesSumNegatives(input) {
+    if(!input || input.length === 0){
+      return []
+    } else{
+      let positiveNum = input.filter((ele) => ele > 0).length 
+    let negativeNum = input.filter((ele) => ele < 0).reduce((acc,val) => acc + val, 0)
+      return [positiveNum, negativeNum]
+    } 
+  }
