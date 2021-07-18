9b7ecae98f61bb8545b707dee46330cd145a585e
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-07-18 13:35:48 -0400

    fixed

--- Count of positives  sum of negatives/index.js
+++ Count of positives  sum of negatives/index.js
@@ -7,7 +7,7 @@
 
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
-
+//SOLUTION
 
 function countPositivesSumNegatives(input) {
     if(!input || input.length === 0){
