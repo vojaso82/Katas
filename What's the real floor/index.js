42f9aee31e586fefc48f4ed622b3884097611426
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-02 15:35:15 -0400

    kata done

--- What's the real floor/index.js
+++ What's the real floor/index.js
@@ -15,3 +15,14 @@
 // 5  =>  4
 // 15  =>  13
 // -3  =>  -3
+
+
+function getRealFloor(n) {
+    if(n === 0){
+    return  0
+  }else if(n < 0 ){
+    return n
+  }else if(n > 0 && n <13){
+    return n-1
+  } else return n-2
+  }
