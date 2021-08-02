d2ae0307fba9c48c7d25dc2fc9d53d75f913842c
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-01 21:52:32 -0400

    done

--- Simple directions reversal/index.js
+++ Simple directions reversal/index.js
@@ -8,3 +8,13 @@
 // Good luck!
 
 // Please also try Simple remove duplicates
+
+function solve(arr){
+    const map = { Begin: 'Begin', Left: 'Right', Right: 'Left' }
+         let parts = arr.map(step => step.split(' on '))
+         let steps = [];
+      for (let i = 0; i < arr.length; i++)
+        steps.unshift(map[parts[(i + 1) % arr.length][0]] + ' on ' + parts[i][1]);
+      return steps;
+    }
+    
