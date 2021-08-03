c87c36c3bb66cb50325ad97551d45e928af82300
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-03 18:28:07 -0400

    done

--- Basic-Mathematical-Operations/index.js
+++ Basic-Mathematical-Operations/index.js
@@ -8,3 +8,20 @@
 // basicOp('-', 15, 18)       // Output: -3
 // basicOp('*', 5, 5)         // Output: 25
 // basicOp('/', 49, 7)        // Output: 7
+
+function basicOp(operation, value1, value2){
+    switch (operation) {
+    case '+':
+      return value1+value2
+      break;
+    case '-':
+      return value1-value2
+      break;
+    case '*':
+      return value1*value2
+      break;
+    case '/':
+      return value1/value2
+      break;
+   }
+  }
