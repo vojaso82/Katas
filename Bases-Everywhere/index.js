3573c5d59a2eee5bdfae9b061baa7d4eaad27d15
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-03 18:21:20 -0400

    test

--- Bases-Everywhere/index.js
+++ Bases-Everywhere/index.js
@@ -1,3 +1,7 @@
 // Uh oh, Someone at the office has dropped all these sequences on the floor and forgotten to label them with their correct bases.
 
 // We have to fix this before the boss gets back or we're all going to be fired!
+// This is what your years of coding have been leading up to, now is your time to shine!
+// Task
+
+// You will have to create a function which takes in a sequence of numbers in random order and you will have to return the correct base of those numbers.
