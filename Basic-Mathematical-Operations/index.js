f8abb58fc9f9223e63822f203c197132069acb6a
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-03 18:27:11 -0400

    test

---
+++ Basic-Mathematical-Operations/index.js
@@ -0,0 +1,3 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number)
