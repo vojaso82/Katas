08396cd88ef399a7964746e8065af7680ecec79a
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-03 18:21:53 -0400

    test

--- Bases-Everywhere/index.js
+++ Bases-Everywhere/index.js
@@ -5,3 +5,5 @@
 // Task
 
 // You will have to create a function which takes in a sequence of numbers in random order and you will have to return the correct base of those numbers.
+// The base is the number of unique digits. For example, a base 10 number can have 10 unique digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 and a base 2 number (Binary) can have 2 unique digits: 0 and 1.
+// Constraints
