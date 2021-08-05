0162b83e8cc06f57abc0e37f36ae0a90532b00f5
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-05 11:47:15 -0400

    done

--- Multiples-of-3-or-5/index.js
+++ Multiples-of-3-or-5/index.js
@@ -2,3 +2,12 @@
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
 //     Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+function solution(number){
+    let sum = 0;
+    for(let i = 0; i < number; i++){
+      if(i % 3 === 0 || i % 5 === 0)
+      sum = sum + i
+      }
+     return sum
+    }
