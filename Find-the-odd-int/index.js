167d37611ac4d021fdd1502e065ad917c1c49358
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-06 11:54:27 -0400

    doing

--- Find-the-odd-int/index.js
+++ Find-the-odd-int/index.js
@@ -1,3 +1,10 @@
 // Given an array of integers, find the one that appears an odd number of times.
 
 // There will always be only one integer that appears an odd number of times.
+
+
+function findOdd(A) {
+    return A.reduce(function(c, v) {
+       return c ^ v;
+     }, 0);
+   }
