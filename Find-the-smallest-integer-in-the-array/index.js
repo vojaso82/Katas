75a8b227b76a731fa396737a10b4f32103e4b157
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-07 11:42:36 -0400

    done

--- Find-the-smallest-integer-in-the-array/index.js
+++ Find-the-smallest-integer-in-the-array/index.js
@@ -7,3 +7,9 @@
 // Given [34, -345, -1, 100] your solution will return -345
 
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
