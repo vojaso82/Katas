6198ead84985dc2cc7a550f9f4ec22a5c9162597
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-07 11:33:33 -0400

    done

--- Highest-and-Lowest/index.js
+++ Highest-and-Lowest/index.js
@@ -4,3 +4,19 @@
 // highAndLow("1 2 3 4 5");  // return "5 1"
 // highAndLow("1 2 -3 4 5"); // return "5 -3"
 // highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes:
+
+//     All numbers are valid Int32, no need to validate them.
+//     There will always be at least one number in the input string.
+//     Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+    if(numbers.split(" ").length === 1){
+       return numbers.concat(" ").repeat(2).trim()
+     } else{
+    
+      return numbers.split(" ").sort((a, b) => b-a).filter((ele, index, arr) => index === 0 || index === arr.length-1).join(" ")
+     
+    //console.log(numbers.split(" ").length)
+     }
+    }
