90ac622cc046e29c37768b75bbde475ab008c35d
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-15 12:18:39 -0400

    solution2

--- Break-camelCase/index.js
+++ Break-camelCase/index.js
@@ -5,7 +5,21 @@
 // "identifier"   =>  "identifier"
 // ""             =>  ""
 
-// complete the function
+// Solution1 with Regex
 function solution(string) {
     return string.replace( /([A-Z])/g, " $1" );
  }
+
+
+// Solution2
+function solution(string) {
+ let newString = "";
+    let letters = string.split("");
+    letters.forEach(letter=>{
+      if(letter.toUpperCase() === letter)
+        newString += " "+letter
+      else
+        newString += letter
+    })
+    return newString;
+}
