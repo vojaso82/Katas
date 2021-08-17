c86262442cbc417a12a33c6c4635acc4cd8d7468
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-17 09:31:43 -0400

    solution2

--- Credit-Card-Mask/index.js
+++ Credit-Card-Mask/index.js
@@ -11,6 +11,8 @@
 // maskify("Skippy")                                   == "##ippy"
 // maskify("Nananananananananananananananana Batman!") == "####################################man!"
 
+
+// Solution1
 function maskify(cc) {
     let string1 = cc.slice(Math.max(cc.length -4, 1))
     let string2 = cc.slice(0, cc.length-4).split("").map(each => each = "#").join("")
@@ -20,4 +22,18 @@ function maskify(cc) {
       return string2+string1
     }
     }
-    
+
+
+    // Solution2
+
+    function maskify(cc) {
+        let string1 = "";
+        for(let i = 0; i < cc.length; i++) {
+          if(i < cc.length - 4) {
+            string1 = string1 + "#";
+          } else {
+            string1 = string1 + cc.charAt(i);
+          };
+        }
+        return string1;
+      }
