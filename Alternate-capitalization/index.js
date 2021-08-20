6c79b4d28e76b1b3d5cc76343b4575420ec72ae0
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-20 10:02:59 -0400

    done

--- Alternate-capitalization/index.js
+++ Alternate-capitalization/index.js
@@ -6,3 +6,22 @@
 
 // Good luck!
 
+function capitalize(s){
+    let finalArray = [];
+    let case1 = s.split("").map((element, index) => {
+      if(index % 2 === 0){
+        return element.toUpperCase()
+       }else{
+        return element.toLowerCase()
+       }
+     }).join("")
+    let case2 = s.split("").map((element, index) => {
+      if(index % 2 !== 0){  
+        return element.toUpperCase()
+       }else{
+        return element.toLowerCase()
+       }
+     }).join("")
+     finalArray.push(case1, case2)
+    return finalArray
+  };
