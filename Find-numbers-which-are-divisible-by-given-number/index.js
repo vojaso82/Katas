8b7f6f51a379d6ebaf42dc7c473c0b9af7a2ebf9
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-24 09:29:24 -0400

    solution2

--- Find-numbers-which-are-divisible-by-given-number/index.js
+++ Find-numbers-which-are-divisible-by-given-number/index.js
@@ -14,3 +14,17 @@ function divisibleBy(numbers, divisor){
       }
      return newNumbers
     }
+
+    //Solution2
+
+    function divisibleBy(numbers, divisor) {
+        let newArr = [];
+        for (let i = 0; i < numbers.length; i++) {
+          if (numbers[i] % divisor === 0) {
+            newArr.push(numbers[i]);
+          } 
+        }
+      return newArr
+      }
+      
+      divisibleBy([1,2,3,4,5,6], 2)
