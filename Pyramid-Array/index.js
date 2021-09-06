28be859708bb96cd977a148814c3d3f12841bf55
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-09-06 11:09:38 -0400

    done

---
+++ Pyramid-Array/index.js
@@ -0,0 +1,17 @@
+// Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+// pyramid(0) => [ ]
+// pyramid(1) => [ [1] ]
+// pyramid(2) => [ [1], [1, 1] ]
+// pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+
+// Note: the subarrays should be filled with 1s
+
+function pyramid(n) {
+    if(n === 0){
+      return []
+    }else{
+    return new Array(n).fill().map((ele,i,arr) => ele = new Array(i+1).fill(1))
+    }
+      
+    }
