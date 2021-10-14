97eb46103eee90a42f82fb483f06832c9b61062d
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-10-14 19:22:43 -0400

    added another solution

--- Highest-and-Lowest/index.js
+++ Highest-and-Lowest/index.js
@@ -29,3 +29,14 @@ function highAndLow(numbers){
         
         return max + ' ' + min
       }
+
+
+
+
+      function highAndLow(numbers) {
+        numbers = numbers.split(' ').map(Number);
+        const max = Math.max(...numbers);
+        const min = Math.min(...numbers);
+        return `${max} ${min}`;
+      }
+      
