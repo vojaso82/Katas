1528cfa14add2e4ec45fb6e95404a6fc4e91667e
vojaso82 <vojislavzaja@gmail.com>
2021-12-09 21:43:37 -0500

    done

---
+++ Shortest Word TS/index.ts
@@ -0,0 +1,11 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+
+function findShort(s){
+    let result = Math.min(...s.split(" ").map((word) => word.length));
+    return result
+    
+  }
